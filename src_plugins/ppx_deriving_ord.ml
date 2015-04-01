22bc5af839ffdbd5f98a67d4e5b7e56eb8c90071
Vincent Berthoux <twinside@gmail.com>
2015-04-01 13:38:24 +0200

    Rejecting type shadowing standard type in ord, with option to allow it.

--- src_plugins/ppx_deriving_ord.ml
+++ src_plugins/ppx_deriving_ord.ml
@@ -5,13 +5,28 @@ open Parsetree
 open Ast_helper
 open Ast_convenience
 
+module StringSet = Ppx_deriving.StringSet
+
 let deriver = "ord"
 let raise_errorf = Ppx_deriving.raise_errorf
 
+type ord_options =
+  {
+    allow_std_type_masking: bool;
+  }
+
+let default_ord_options =
+  {
+    allow_std_type_masking = false;
+  }
+
 let parse_options options =
-  options |> List.iter (fun (name, expr) ->
+  let option_parser acc (name, expr) =
     match name with
-    | _ -> raise_errorf ~loc:expr.pexp_loc "%s does not support option %s" deriver name)
+    | "allow_std_type_masking" -> { allow_std_type_masking = true }
+    | _ ->
+      raise_errorf ~loc:expr.pexp_loc "%s does not support option %s" deriver name in
+  List.fold_left option_parser default_ord_options options
 
 let attr_compare attrs =
   Ppx_deriving.(attrs |> attr ~deriver "compare" |> Arg.(get_attr ~deriver expr))
@@ -34,91 +49,98 @@ let wildcard_case int_cases =
 let pattn side typs =
   List.mapi (fun i _ -> pvar (argn side i)) typs
 
-let rec exprsn typs =
+let rec exprsn group_def typs =
   typs |> List.mapi (fun i typ ->
-    app (expr_of_typ typ) [evar (argn `lhs i); evar (argn `rhs i)])
+    app (expr_of_typ group_def typ) [evar (argn `lhs i); evar (argn `rhs i)])
 
-and expr_of_typ typ =
-  match attr_compare typ.ptyp_attributes with
-  | Some fn -> fn
-  | None ->
-    match typ with
-    | [%type: _] | [%type: unit] -> [%expr fun _ _ -> 0]
-    | [%type: int] | [%type: int32] | [%type: Int32.t]
-    | [%type: int64] | [%type: Int64.t] | [%type: nativeint] | [%type: Nativeint.t]
-    | [%type: float] | [%type: bool] | [%type: char] | [%type: string] | [%type: bytes] ->
-      [%expr Pervasives.compare]
-    | [%type: [%t? typ] ref]   -> [%expr fun a b -> [%e expr_of_typ typ] !a !b]
-    | [%type: [%t? typ] list]  ->
-      [%expr
-        let rec loop x y =
+and expr_of_typ group_def typ =
+  let rec aux typ =
+    match attr_compare typ.ptyp_attributes with
+    | Some fn -> fn
+    | None ->
+      match typ with
+      | { ptyp_desc = Ptyp_constr ({ txt = (Lident id as lid) }, args) } when
+            StringSet.mem id group_def ->
+        let compare_fn = Exp.ident (mknoloc (Ppx_deriving.mangle_lid (`Prefix "compare") lid)) in
+        app compare_fn (List.map aux args)
+      | [%type: _] | [%type: unit] -> [%expr fun _ _ -> 0]
+      | [%type: int] | [%type: int32] | [%type: Int32.t]
+      | [%type: int64] | [%type: Int64.t] | [%type: nativeint] | [%type: Nativeint.t]
+      | [%type: float] | [%type: bool] | [%type: char] | [%type: string] | [%type: String.t] |
+        [%type: bytes] ->
+        [%expr Pervasives.compare]
+      | [%type: [%t? typ] ref]   -> [%expr fun a b -> [%e aux typ] !a !b]
+      | [%type: [%t? typ] list]  ->
+        [%expr
+          let rec loop x y =
+            match x, y with
+            | [], [] -> 0
+            | [], _ -> -1
+            | _, [] -> 1
+            | a :: x, b :: y ->
+              [%e compare_reduce [%expr loop x y] [%expr [%e aux typ] a b]]
+          in (fun x y -> loop x y)]
+      | [%type: [%t? typ] array] ->
+        [%expr fun x y ->
+          let rec loop i =
+            if i = Array.length x then 0
+            else [%e compare_reduce [%expr loop (i + 1)]
+                                    [%expr [%e aux typ] x.(i) y.(i)]]
+          in
+          [%e compare_reduce [%expr loop 0]
+                             [%expr Pervasives.compare (Array.length x) (Array.length y)]]]
+      | [%type: [%t? typ] option] ->
+        [%expr fun x y ->
           match x, y with
-          | [], [] -> 0
-          | [], _ -> -1
-          | _, [] -> 1
-          | a :: x, b :: y ->
-            [%e compare_reduce [%expr loop x y] [%expr [%e expr_of_typ typ] a b]]
-        in (fun x y -> loop x y)]
-    | [%type: [%t? typ] array] ->
-      [%expr fun x y ->
-        let rec loop i =
-          if i = Array.length x then 0
-          else [%e compare_reduce [%expr loop (i + 1)]
-                                  [%expr [%e expr_of_typ typ] x.(i) y.(i)]]
+          | None, None -> 0
+          | Some a, Some b -> [%e aux typ] a b
+          | None, Some _ -> -1
+          | Some _, None -> 1]
+      | { ptyp_desc = Ptyp_constr ({ txt = lid }, args) } ->
+        let compare_fn = Exp.ident (mknoloc (Ppx_deriving.mangle_lid (`Prefix "compare") lid)) in
+        app compare_fn (List.map aux args)
+      | { ptyp_desc = Ptyp_tuple typs } ->
+        [%expr fun [%p ptuple (pattn `lhs typs)] [%p ptuple (pattn `rhs typs)] ->
+          [%e exprsn group_def typs |> List.rev |> reduce_compare]]
+      | { ptyp_desc = Ptyp_variant (fields, _, _); ptyp_loc } ->
+        let cases =
+          fields |> List.map (fun field ->
+            let pdup f = ptuple [f "lhs"; f "rhs"] in
+            match field with
+            | Rtag (label, _, true (*empty*), []) ->
+              Exp.case (pdup (fun _ -> Pat.variant label None)) [%expr 0]
+            | Rtag (label, _, false, [typ]) ->
+              Exp.case (pdup (fun var -> Pat.variant label (Some (pvar var))))
+                       (app (aux typ) [evar "lhs"; evar "rhs"])
+            | Rinherit ({ ptyp_desc = Ptyp_constr (tname, _) } as typ) ->
+              Exp.case (pdup (fun var -> Pat.alias (Pat.type_ tname) (mknoloc var)))
+                       (app (aux typ) [evar "lhs"; evar "rhs"])
+            | _ ->
+              raise_errorf ~loc:ptyp_loc "%s cannot be derived for %s"
+                           deriver (Ppx_deriving.string_of_core_type typ))
         in
-        [%e compare_reduce [%expr loop 0]
-                           [%expr Pervasives.compare (Array.length x) (Array.length y)]]]
-    | [%type: [%t? typ] option] ->
-      [%expr fun x y ->
-        match x, y with
-        | None, None -> 0
-        | Some a, Some b -> [%e expr_of_typ typ] a b
-        | None, Some _ -> -1
-        | Some _, None -> 1]
-    | { ptyp_desc = Ptyp_constr ({ txt = lid }, args) } ->
-      let compare_fn = Exp.ident (mknoloc (Ppx_deriving.mangle_lid (`Prefix "compare") lid)) in
-      app compare_fn (List.map expr_of_typ args)
-    | { ptyp_desc = Ptyp_tuple typs } ->
-      [%expr fun [%p ptuple (pattn `lhs typs)] [%p ptuple (pattn `rhs typs)] ->
-        [%e exprsn typs |> List.rev |> reduce_compare]]
-    | { ptyp_desc = Ptyp_variant (fields, _, _); ptyp_loc } ->
-      let cases =
-        fields |> List.map (fun field ->
-          let pdup f = ptuple [f "lhs"; f "rhs"] in
-          match field with
-          | Rtag (label, _, true (*empty*), []) ->
-            Exp.case (pdup (fun _ -> Pat.variant label None)) [%expr 0]
-          | Rtag (label, _, false, [typ]) ->
-            Exp.case (pdup (fun var -> Pat.variant label (Some (pvar var))))
-                     (app (expr_of_typ typ) [evar "lhs"; evar "rhs"])
-          | Rinherit ({ ptyp_desc = Ptyp_constr (tname, _) } as typ) ->
-            Exp.case (pdup (fun var -> Pat.alias (Pat.type_ tname) (mknoloc var)))
-                     (app (expr_of_typ typ) [evar "lhs"; evar "rhs"])
-          | _ ->
-            raise_errorf ~loc:ptyp_loc "%s cannot be derived for %s"
-                         deriver (Ppx_deriving.string_of_core_type typ))
-      in
-      let int_cases =
-        fields |> List.mapi (fun i field ->
-          match field with
-          | Rtag (label, _, true (*empty*), []) ->
-            Exp.case (Pat.variant label None) (int i)
-          | Rtag (label, _, false, [typ]) ->
-            Exp.case (Pat.variant label (Some [%pat? _])) (int i)
-          | Rinherit { ptyp_desc = Ptyp_constr (tname, []) } ->
-            Exp.case (Pat.type_ tname) (int i)
-          | _ -> assert false)
-      in
-      [%expr fun lhs rhs ->
-        [%e Exp.match_ [%expr lhs, rhs] (cases @ [wildcard_case int_cases])]]
-    | { ptyp_desc = Ptyp_var name } -> evar ("poly_"^name)
-    | { ptyp_desc = Ptyp_alias (typ, _) } -> expr_of_typ typ
-    | { ptyp_loc } ->
-      raise_errorf ~loc:ptyp_loc "%s cannot be derived for %s"
-                   deriver (Ppx_deriving.string_of_core_type typ)
+        let int_cases =
+          fields |> List.mapi (fun i field ->
+            match field with
+            | Rtag (label, _, true (*empty*), []) ->
+              Exp.case (Pat.variant label None) (int i)
+            | Rtag (label, _, false, [typ]) ->
+              Exp.case (Pat.variant label (Some [%pat? _])) (int i)
+            | Rinherit { ptyp_desc = Ptyp_constr (tname, []) } ->
+              Exp.case (Pat.type_ tname) (int i)
+            | _ -> assert false)
+        in
+        [%expr fun lhs rhs ->
+          [%e Exp.match_ [%expr lhs, rhs] (cases @ [wildcard_case int_cases])]]
+      | { ptyp_desc = Ptyp_var name } -> evar ("poly_"^name)
+      | { ptyp_desc = Ptyp_alias (typ, _) } -> aux typ
+      | { ptyp_loc } ->
+        raise_errorf ~loc:ptyp_loc "%s cannot be derived for %s"
+                     deriver (Ppx_deriving.string_of_core_type typ) in
+  aux typ
 
 let core_type_of_decl ~options ~path type_decl =
-  parse_options options;
+  ignore (parse_options options);
   let typ = Ppx_deriving.core_type_of_type_decl type_decl in
   let polymorphize = Ppx_deriving.poly_arrow_of_type_decl
           (fun var -> [%type: [%t var] -> [%t var] -> int]) type_decl in
@@ -128,11 +150,11 @@ let sig_of_type ~options ~path type_decl =
   [Sig.value (Val.mk (mknoloc (Ppx_deriving.mangle_type_decl (`Prefix "compare") type_decl))
              (core_type_of_decl ~options ~path type_decl))]
 
-let str_of_type ~options ~path ({ ptype_loc = loc } as type_decl) =
-  parse_options options;
+let str_of_type ~options ~path group_def ({ ptype_loc = loc } as type_decl) =
+  ignore (parse_options options);
   let comparator =
     match type_decl.ptype_kind, type_decl.ptype_manifest with
-    | Ptype_abstract, Some manifest -> expr_of_typ manifest
+    | Ptype_abstract, Some manifest -> expr_of_typ group_def manifest
     | Ptype_variant constrs, _ ->
       let int_cases =
           constrs |> List.mapi (fun i { pcd_name = { txt = name }; pcd_args } ->
@@ -141,7 +163,7 @@ let str_of_type ~options ~path ({ ptype_loc = loc } as type_decl) =
             | _  -> Exp.case (pconstr name (List.map (fun _ -> [%pat? _]) pcd_args)) (int i))
       and cases =
         constrs |> List.map (fun { pcd_name = { txt = name }; pcd_args = typs } ->
-          exprsn typs |> List.rev |> reduce_compare |>
+          exprsn group_def typs |> List.rev |> reduce_compare |>
           Exp.case (ptuple [pconstr name (pattn `lhs typs);
                             pconstr name (pattn `rhs typs)]))
       in
@@ -151,7 +173,7 @@ let str_of_type ~options ~path ({ ptype_loc = loc } as type_decl) =
       let exprs =
         labels |> List.map (fun { pld_name = { txt = name }; pld_type } ->
           let field obj = Exp.field obj (mknoloc (Lident name)) in
-          app (expr_of_typ pld_type) [field (evar "lhs"); field (evar "rhs")])
+          app (expr_of_typ group_def pld_type) [field (evar "lhs"); field (evar "rhs")])
       in
       [%expr fun lhs rhs -> [%e reduce_compare exprs]]
     | Ptype_abstract, None ->
@@ -168,11 +190,21 @@ let str_of_type ~options ~path ({ ptype_loc = loc } as type_decl) =
   [Vb.mk (Pat.constraint_ out_var out_type)
          (polymorphize comparator)]
 
+let type_decl_str ~options ~path type_decls =
+  let opts = parse_options options in
+  let typename_set =
+    Ppx_deriving.extract_typename_of_type_group 
+      deriver
+      ~allow_shadowing:opts.allow_std_type_masking
+      type_decls in
+  let code =
+    List.map (str_of_type ~options ~path typename_set) type_decls in
+  [Str.value Recursive (List.concat code)]
+
 let () =
   Ppx_deriving.(register (create deriver
-    ~core_type: expr_of_typ
-    ~type_decl_str: (fun ~options ~path type_decls ->
-      [Str.value Recursive (List.concat (List.map (str_of_type ~options ~path) type_decls))])
+    ~core_type: (expr_of_typ StringSet.empty)
+    ~type_decl_str: type_decl_str
     ~type_decl_sig: (fun ~options ~path type_decls ->
       List.concat (List.map (sig_of_type ~options ~path) type_decls))
     ()
