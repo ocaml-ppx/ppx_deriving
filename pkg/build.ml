ocaml-ppx/ppx_deriving

commit 8f871996b5cc7bfba676f18e3adea5eff05182b5

preparing to release 2.1, update changelog

pkg/META.in
@@ -1,6 +1,6 @@
 version = "%{version}%"
 description = "Type-driven code generation"
-ppx = "./ppx_deriving"
+ppx = "./ppx_deriving_main"
 
 package "api" (
   version = "%{version}%"

commit f69ac6dc503d2cda6bd79c417228b05cb2a1a252

Rename installed binary back to ppx_deriving.
This fixes 6ccfb591, which tried to fix an error introduced
in 8f871996.

pkg/META.in
@@ -1,6 +1,6 @@
 version = "%{version}%"
 description = "Type-driven code generation"
-ppx = "./ppx_deriving_main"
+ppx = "./ppx_deriving"
 
 package "api" (
   version = "%{version}%"

pkg/build.ml
@@ -10,7 +10,7 @@ let () =
 let () =
   Pkg.describe "ppx_deriving" ~builder:`OCamlbuild [
     Pkg.lib "pkg/META";
-    Pkg.bin ~auto:true "src/ppx_deriving_main" ~dst:"../lib/ppx_deriving/ppx_deriving_main";
+    Pkg.bin ~auto:true "src/ppx_deriving_main" ~dst:"../lib/ppx_deriving/ppx_deriving";
     Pkg.lib ~exts:Exts.module_library "src/ppx_deriving";
     Pkg.lib ~exts:Exts.library "src/ppx_deriving_main";
     Pkg.lib ~exts:Exts.library "src_plugins/ppx_deriving_show";
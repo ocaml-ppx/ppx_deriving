f69ac6dc503d2cda6bd79c417228b05cb2a1a252
whitequark <whitequark@whitequark.org>
2015-07-03 18:21:38 +0300

    Rename installed binary back to ppx_deriving.
    This fixes 6ccfb591, which tried to fix an error introduced
    in 8f871996.

--- pkg/build.ml
+++ pkg/build.ml
@@ -10,7 +10,7 @@ let () =
 let () =
   Pkg.describe "ppx_deriving" ~builder:`OCamlbuild [
     Pkg.lib "pkg/META";
-    Pkg.bin ~auto:true "src/ppx_deriving_main" ~dst:"../lib/ppx_deriving/ppx_deriving_main";
+    Pkg.bin ~auto:true "src/ppx_deriving_main" ~dst:"../lib/ppx_deriving/ppx_deriving";
     Pkg.lib ~exts:Exts.module_library "src/ppx_deriving";
     Pkg.lib ~exts:Exts.library "src/ppx_deriving_main";
     Pkg.lib ~exts:Exts.library "src_plugins/ppx_deriving_show";
