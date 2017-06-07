6b75b9e4804b253945ab61a76ed4af539d6caf90
Jeremie Dimino <jdimino@janestreet.com>
2017-06-07 13:36:43 +0100

    Revert "Use ocaml-migrate-parsetree instead of #ifdef hell."
    This reverts commit 87050888d4fe8b12f667cfb91360e9292d344ec8.

--- src_plugins/ppx_deriving_map.cppo.ml
+++ src_plugins/ppx_deriving_map.cppo.ml
@@ -1,3 +1,7 @@
+#if OCAML_VERSION < (4, 03, 0)
+#define Pcstr_tuple(core_types) core_types
+#endif
+
 open Longident
 open Location
 open Asttypes
@@ -5,7 +9,7 @@ open Parsetree
 open Ast_helper
 open Ast_convenience
 
-let deriver = "fold"
+let deriver = "map"
 let raise_errorf = Ppx_deriving.raise_errorf
 
 let parse_options options =
@@ -23,51 +27,52 @@ let pattn typs   = List.mapi (fun i _ -> pvar (argn i)) typs
 let pattl labels = List.map (fun { pld_name = { txt = n } } -> n, pvar (argl n)) labels
 
 let pconstrrec name fields = pconstr name [precord ~closed:Closed fields]
+let  constrrec name fields =  constr name [ record                fields]
 
-let reduce_acc a b = [%expr let acc = [%e a] in [%e b]]
-
-let rec expr_of_typ typ =
+let rec expr_of_typ ?decl typ =
   let typ = Ppx_deriving.remove_pervasives ~deriver typ in
   match typ with
-  | _ when Ppx_deriving.free_vars_in_core_type typ = [] -> [%expr fun acc _ -> acc]
-  | { ptyp_desc = Ptyp_constr ({ txt = lid }, args) } ->
+  | _ when Ppx_deriving.free_vars_in_core_type typ = [] -> [%expr fun x -> x]
+  | { ptyp_desc = Ptyp_constr _ } ->
     let builtin = not (attr_nobuiltin typ.ptyp_attributes) in
     begin match builtin, typ with
-    | true, [%type: [%t? typ] ref] -> [%expr fun acc x -> [%e expr_of_typ typ] acc !x]
     | true, [%type: [%t? typ] list] ->
-      [%expr Ppx_deriving_runtime.List.fold_left [%e expr_of_typ typ]]
+      [%expr Ppx_deriving_runtime.List.map [%e expr_of_typ ?decl typ]]
     | true, [%type: [%t? typ] array] ->
-      [%expr Ppx_deriving_runtime.Array.fold_left [%e expr_of_typ typ]]
+      [%expr Ppx_deriving_runtime.Array.map [%e expr_of_typ ?decl typ]]
     | true, [%type: [%t? typ] option] ->
-      [%expr fun acc -> function None -> acc | Some x -> [%e expr_of_typ typ] acc x]
+      [%expr function None -> None | Some x -> Some ([%e expr_of_typ ?decl typ] x)]
     | true, ([%type: ([%t? ok_t], [%t? err_t]) result] |
              [%type: ([%t? ok_t], [%t? err_t]) Result.result]) ->
       [%expr
-        fun acc -> function
-        | Result.Ok ok -> [%e expr_of_typ ok_t] acc ok
-        | Result.Error err -> [%e expr_of_typ err_t] acc err]
+        function
+        | Result.Ok ok -> Result.Ok ([%e expr_of_typ ?decl ok_t] ok)
+        | Result.Error err -> Result.Error ([%e expr_of_typ ?decl err_t] err)]
     | _, { ptyp_desc = Ptyp_constr ({ txt = lid }, args) } ->
       app (Exp.ident (mknoloc (Ppx_deriving.mangle_lid (`Prefix deriver) lid)))
-          (List.map expr_of_typ args)
+          (List.map (expr_of_typ ?decl) args)
     | _ -> assert false
     end
   | { ptyp_desc = Ptyp_tuple typs } ->
-    let args = typs |> List.mapi (fun i typ ->
-                  [%expr [%e expr_of_typ typ] acc [%e evar (argn i)]]) in
-    [%expr fun acc [%p ptuple (List.mapi (fun i _ -> pvar (argn i)) typs)] ->
-      [%e Ppx_deriving.(fold_exprs ~unit:[%expr acc] reduce_acc args)]];
+    [%expr fun [%p ptuple (List.mapi (fun i _ -> pvar (argn i)) typs)] ->
+      [%e tuple (List.mapi (fun i typ -> app (expr_of_typ ?decl typ) [evar (argn i)]) typs)]];
   | { ptyp_desc = Ptyp_variant (fields, _, _); ptyp_loc } ->
     let cases =
       fields |> List.map (fun field ->
         match field with
         | Rtag (label, _, true (*empty*), []) ->
-          Exp.case (Pat.variant label None) [%expr acc]
+          Exp.case (Pat.variant label None) (Exp.variant label None)
         | Rtag (label, _, false, [typ]) ->
           Exp.case (Pat.variant label (Some [%pat? x]))
-                   [%expr [%e expr_of_typ typ] acc x]
-        | Rinherit ({ ptyp_desc = Ptyp_constr (tname, _) } as typ) ->
-          Exp.case [%pat? [%p Pat.type_ tname] as x]
-                   [%expr [%e expr_of_typ typ] acc x]
+                   (Exp.variant label (Some [%expr [%e expr_of_typ ?decl typ] x]))
+        | Rinherit ({ ptyp_desc = Ptyp_constr (tname, _) } as typ) -> begin
+          match decl with
+          | None -> 
+            raise_errorf "inheritance of polymorphic variants not supported"
+          | Some(d) ->
+            Exp.case [%pat? [%p Pat.type_ tname] as x]
+                     [%expr ([%e expr_of_typ ?decl typ] x :> [%t Ppx_deriving.core_type_of_type_decl d])]
+          end
         | _ ->
           raise_errorf ~loc:ptyp_loc "%s cannot be derived for %s"
                        deriver (Ppx_deriving.string_of_core_type typ))
@@ -75,7 +80,7 @@ let rec expr_of_typ typ =
     Exp.function_ cases
   | { ptyp_desc = Ptyp_var name } -> evar ("poly_"^name)
   | { ptyp_desc = Ptyp_alias (typ, name) } ->
-    [%expr fun acc x -> [%e evar ("poly_"^name)] ([%e expr_of_typ typ] acc x) x]
+    [%expr fun x -> [%e evar ("poly_"^name)] ([%e expr_of_typ ?decl typ] x)]
   | { ptyp_loc } ->
     raise_errorf ~loc:ptyp_loc "%s cannot be derived for %s"
                  deriver (Ppx_deriving.string_of_core_type typ)
@@ -84,30 +89,31 @@ let str_of_type ~options ~path ({ ptype_loc = loc } as type_decl) =
   parse_options options;
   let mapper =
     match type_decl.ptype_kind, type_decl.ptype_manifest with
-    | Ptype_abstract, Some manifest -> expr_of_typ manifest
+    | Ptype_abstract, Some manifest -> expr_of_typ ~decl:type_decl manifest
     | Ptype_variant constrs, _ ->
-      let cases = constrs |> List.map (fun { pcd_name = { txt = name' }; pcd_args } ->
+      constrs |>
+      List.map (fun { pcd_name = { txt = name' }; pcd_args } ->
         match pcd_args with
         | Pcstr_tuple(typs) ->
-          let args = typs |> List.mapi (fun i typ ->
-                        [%expr [%e expr_of_typ typ] acc [%e evar (argn i)]]) in
+          let args = List.mapi (fun i typ -> app (expr_of_typ ~decl:type_decl typ) [evar (argn i)]) typs in
           Exp.case (pconstr name' (pattn typs))
-                   Ppx_deriving.(fold_exprs ~unit:[%expr acc] reduce_acc args)
+                   (constr name' args)
+#if OCAML_VERSION >= (4, 03, 0)
         | Pcstr_record(labels) ->
           let args = labels |> List.map (fun { pld_name = { txt = n }; pld_type = typ } ->
-                        [%expr [%e expr_of_typ typ] acc [%e evar (argl n)]]) in
+                        n, [%expr [%e expr_of_typ ~decl:type_decl typ] [%e evar (argl n)]]) in
           Exp.case (pconstrrec name' (pattl labels))
-                   Ppx_deriving.(fold_exprs ~unit:[%expr acc] reduce_acc args)
-        )
-      in
-      [%expr fun acc -> [%e Exp.function_ cases]]
+                   (constrrec name' args)
+#endif
+        ) |>
+      Exp.function_
     | Ptype_record labels, _ ->
       let fields =
         labels |> List.mapi (fun i { pld_name = { txt = name }; pld_type } ->
-          [%expr [%e expr_of_typ pld_type] acc
-                 [%e Exp.field (evar "x") (mknoloc (Lident name))]])
+          name, [%expr [%e expr_of_typ ~decl:type_decl pld_type]
+                       [%e Exp.field (evar "x") (mknoloc (Lident name))]])
       in
-      [%expr fun acc x -> [%e Ppx_deriving.(fold_exprs ~unit:[%expr acc] reduce_acc fields)]]
+      [%expr fun x -> [%e record fields]]
     | Ptype_abstract, None ->
       raise_errorf ~loc "%s cannot be derived for fully abstract types" deriver
     | Ptype_open, _        ->
@@ -120,16 +126,17 @@ let str_of_type ~options ~path ({ ptype_loc = loc } as type_decl) =
 
 let sig_of_type ~options ~path type_decl =
   parse_options options;
-  let typ = Ppx_deriving.core_type_of_type_decl type_decl in
-  let acc = Typ.var Ppx_deriving.(fresh_var (free_vars_in_core_type typ)) in
-  let polymorphize = Ppx_deriving.poly_arrow_of_type_decl
-                        (fun var -> [%type: [%t acc] -> [%t var] -> [%t acc]]) type_decl in
-  [Sig.value (Val.mk (mknoloc (Ppx_deriving.mangle_type_decl (`Prefix deriver) type_decl))
-              (polymorphize [%type: [%t acc] -> [%t typ] -> [%t acc]]))]
+  let typ_arg, var_arg, bound = Ppx_deriving.instantiate []    type_decl in
+  let typ_ret, var_ret, _     = Ppx_deriving.instantiate bound type_decl in
+  let arrow = Typ.arrow Label.nolabel in
+  let poly_fns = List.map2 (fun a r -> [%type: [%t Typ.var a] -> [%t Typ.var r]])
+                           var_arg var_ret in
+  let typ = List.fold_right arrow poly_fns (arrow typ_arg typ_ret) in
+  [Sig.value (Val.mk (mknoloc (Ppx_deriving.mangle_type_decl (`Prefix deriver) type_decl)) typ)]
 
 let () =
   Ppx_deriving.(register (create deriver
-    ~core_type: expr_of_typ
+    ~core_type: (expr_of_typ ?decl:None)
     ~type_decl_str: (fun ~options ~path type_decls ->
       [Str.value Recursive (List.concat (List.map (str_of_type ~options ~path) type_decls))])
     ~type_decl_sig: (fun ~options ~path type_decls ->
--- src_plugins/ppx_deriving_show.cppo.ml
+++ src_plugins/ppx_deriving_show.cppo.ml
@@ -1,3 +1,7 @@
+#if OCAML_VERSION < (4, 03, 0)
+#define Pcstr_tuple(core_types) core_types
+#endif
+
 open Longident
 open Location
 open Asttypes
@@ -216,10 +220,12 @@ let str_of_type ~options ~path ({ ptype_loc = loc } as type_decl) =
             Exp.case (pconstr name' pattern_vars)
               [%expr [%e wrap_printer quoter printer] fmt
                         [%e tuple expr_vars]]
+#if OCAML_VERSION >= (4, 03, 0)
           | Some printer, Pcstr_record(labels) ->
             let args = labels |> List.map (fun { pld_name = { txt = n } } -> evar (argl n)) in
             Exp.case (pconstrrec name' (pattl labels))
                      (app (wrap_printer quoter printer) ([%expr fmt] :: args))
+#endif
           | None, Pcstr_tuple(typs) ->
             let args =
               List.mapi (fun i typ -> app (expr_of_typ quoter typ) [evar (argn i)]) typs in
@@ -239,6 +245,7 @@ let str_of_type ~options ~path ({ ptype_loc = loc } as type_decl) =
                   Format.fprintf fmt "@,))@]"]
             in
             Exp.case (pconstr name' (pattn typs)) printer
+#if OCAML_VERSION >= (4, 03, 0)
           | None, Pcstr_record(labels) ->
             let args =
               labels |> List.map (fun { pld_name = { txt = n }; pld_type = typ } ->
@@ -256,6 +263,7 @@ let str_of_type ~options ~path ({ ptype_loc = loc } as type_decl) =
                 Format.fprintf fmt "@]}"]
             in
             Exp.case (pconstrrec name' (pattl labels)) printer
+#endif
           )
       in
       [%expr fun fmt -> [%e Exp.function_ cases]]
