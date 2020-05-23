c9ba2c2fd21571dfdb41f85329113bbe846e7290
Thierry Martinez <Thierry.Martinez@inria.fr>
2020-05-24 00:32:34 +0200

    Use "include module type of struct include" to get all equalities
    Some equalities were missing, for instance the equality between
    `Pervasives.result` and `Result.result`.
    
    Instead of trying to enumerate all equalities explicitly, this
    commit proposes to use the construction
    
    ```
    include module type of struct
      include M
    end
    ```
    
    to precisely get the signature of `include M`.
    
    Moreover, it seems that we do not lose anything by using module
    aliases in signatures.

--- src/runtime/ppx_deriving_runtime.cppo.mli
+++ src/runtime/ppx_deriving_runtime.cppo.mli
@@ -22,81 +22,53 @@ type nonrec bytes = bytes
 (** {2 Predefined modules}
     {3 Operations on predefined types} *)
 
-#if OCAML_VERSION >= (4, 08, 0)
-include (module type of Stdlib with
-  type fpclass = Stdlib.fpclass and
-  type in_channel = Stdlib.in_channel and
-  type out_channel = Stdlib.out_channel and
-  type open_flag = Stdlib.open_flag and
-  type 'a ref = 'a Stdlib.ref and
-  type ('a, 'b, 'c, 'd, 'e, 'f) format6 = ('a, 'b, 'c, 'd, 'e, 'f) Stdlib.format6 and
-  type ('a, 'b, 'c, 'd) format4 = ('a, 'b, 'c, 'd) Stdlib.format4 and
-  type ('a, 'b, 'c) format = ('a, 'b, 'c) Stdlib.format
-)
+#if OCAML_VERSION >= (4, 07, 0)
+include module type of struct
+  include Stdlib
+end
 
 module Result : sig
-  type ('a, 'b) t = ('a, 'b) Result.t =
+  type ('a, 'b) t = ('a, 'b) result =
     | Ok of 'a
     | Error of 'b
 
   (* we also expose Result.result for backward-compatibility
      with the Result package! *)
-  type ('a, 'b) result = ('a, 'b) Result.t =
+  type ('a, 'b) result = ('a, 'b) t =
     | Ok of 'a
     | Error of 'b
 end
 #else
-module Pervasives : (module type of Pervasives with
-  type fpclass = Pervasives.fpclass and
-  type in_channel = Pervasives.in_channel and
-  type out_channel = Pervasives.out_channel and
-  type open_flag = Pervasives.open_flag and
-  type 'a ref = 'a Pervasives.ref and
-  type ('a, 'b, 'c, 'd, 'e, 'f) format6 = ('a, 'b, 'c, 'd, 'e, 'f) Pervasives.format6 and
-  type ('a, 'b, 'c, 'd) format4 = ('a, 'b, 'c, 'd) Pervasives.format4 and
-  type ('a, 'b, 'c) format = ('a, 'b, 'c) Pervasives.format)
+module Pervasives = Pervasives
 
 module Stdlib = Pervasives
 
-include (module type of Pervasives with
-  type fpclass = Pervasives.fpclass and
-  type in_channel = Pervasives.in_channel and
-  type out_channel = Pervasives.out_channel and
-  type open_flag = Pervasives.open_flag and
-  type 'a ref = 'a Pervasives.ref and
-  type ('a, 'b, 'c, 'd, 'e, 'f) format6 = ('a, 'b, 'c, 'd, 'e, 'f) Pervasives.format6 and
-  type ('a, 'b, 'c, 'd) format4 = ('a, 'b, 'c, 'd) Pervasives.format4 and
-  type ('a, 'b, 'c) format = ('a, 'b, 'c) Pervasives.format)
+include module type of struct
+  include Pervasives
+end
 
-module Char : (module type of Char)
-module String : (module type of String)
-module Printexc : (module type of Printexc with
-  type raw_backtrace = Printexc.raw_backtrace and
-  type backtrace_slot = Printexc.backtrace_slot and
-  type location = Printexc.location)
-module Array : (module type of Array)
-module List : (module type of List)
-module Nativeint : (module type of Nativeint)
-module Int32 : (module type of Int32)
-module Int64 : (module type of Int64)
-module Lazy : (module type of Lazy)
-module Bytes : (module type of Bytes)
+module Char = Char
+module String = String
+module Printexc = Printexc
+module Array = Array
+module List = List
+module Nativeint = Nativeint
+module Int32 = Int32
+module Int64 = Int64
+module Lazy = Lazy
+module Bytes = Bytes
 
-(** {3 Data structures} *)
+module Hashtbl = Hashtbl
+module Queue = Queue
+module Stack = Stack
+module Set = Set
+module Map = Map
+module Weak = Weak
 
-module Hashtbl : (module type of Hashtbl with
-  type ('a, 'b) t = ('a, 'b) Hashtbl.t and
-  type statistics = Hashtbl.statistics)
-module Queue : (module type of Queue with
-  type 'a t = 'a Queue.t)
-module Stack : (module type of Stack with
-  type 'a t = 'a Stack.t)
-module Set : (module type of Set)
-module Map : (module type of Map)
-module Weak : (module type of Weak with
-  type 'a t = 'a Weak.t)
-module Buffer : (module type of Buffer with
-  type t = Buffer.t)
+module Printf = Printf
+module Format = Format
+module Buffer = Buffer
+
 module Result : sig
   type ('a, 'b) t = ('a, 'b) Result.result =
     | Ok of 'a
@@ -107,12 +79,4 @@ module Result : sig
     | Ok of 'a
     | Error of 'b
 end
-
-(** {3 Formatting} *)
-
-module Printf : (module type of Printf)
-module Format : (module type of Format with
-  type formatter_out_functions = Format.formatter_out_functions and
-  type formatter_tag_functions = Format.formatter_tag_functions and
-  type formatter = Format.formatter)
 #endif
