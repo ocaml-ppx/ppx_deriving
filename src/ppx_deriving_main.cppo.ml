d4c767df3317dfcb3d07176b542f834e193d8565
Jeremie Dimino <jeremie@dimino.org>
2020-11-12 12:30:04 +0000

    fixed loader
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- src/ppx_deriving_main.cppo.ml
+++ src/ppx_deriving_main.cppo.ml
@@ -68,32 +68,30 @@ let add_plugins plugins =
 let mapper argv =
   get_plugins () |> List.iter load_plugin;
   add_plugins argv;
-  let copy_structure_item item =
-    match From_current.copy_structure [item] with
-    | [item] -> item
-    | _ -> failwith "Ppx_deriving_main.copy_structure_item" in
   let module Current_ast = Ppxlib_ast.Selected_ast in
-  let omp_mapper = Migrate_parsetree.Driver.run_as_ast_mapper [] in
-  let structure mapper s =
+  let structure s =
     match s with
     | [] -> []
     | hd :: tl ->
-        match
-          try Some (copy_structure_item hd)
-          with Migrate_parsetree.Def.Migration_error (_, _) -> None
-        with
-        | Some ([%stri [@@@findlib.ppxopt [%e? { pexp_desc = Pexp_tuple (
-            [%expr "ppx_deriving"] :: elems) }]]]) ->
-            elems |>
-            List.map (fun elem ->
-              match elem with
-              | { pexp_desc = Pexp_constant (Pconst_string (file, None))} ->
-                  file
-              | _ -> assert false) |>
-            add_plugins;
-            mapper.Current_ast.Ast_mapper.structure mapper tl
-        | _ -> omp_mapper.Current_ast.Ast_mapper.structure mapper s in
-  { omp_mapper with Current_ast.Ast_mapper.structure }
+      match hd with
+      | [%stri [@@@findlib.ppxopt [%e? { pexp_desc = Pexp_tuple (
+          [%expr "ppx_deriving"] :: elems) }]]] ->
+        elems |>
+        List.map (fun elem ->
+            match elem with
+            | { pexp_desc = Pexp_constant (Pconst_string (file, None))} ->
+              file
+            | _ -> assert false) |>
+        add_plugins;
+        Ppxlib.Driver.map_structure tl
+      | _ -> Ppxlib.Driver.map_structure s
+  in
+  let structure _ st =
+    Current_ast.of_ocaml Structure st
+    |> structure
+    |> Current_ast.to_ocaml Structure
+  in
+  { Ast_mapper.default_mapper with structure }
 
 let () =
   Ast_mapper.register "ppx_deriving" mapper
